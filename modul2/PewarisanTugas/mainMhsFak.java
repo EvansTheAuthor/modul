d3d0e3ea34819f1228ef551fec1afafa7c33d2d2
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-09-12 13:00:09 +0700

    Update mainMhsFak.java

--- modul2/PewarisanTugas/mainMhsFak.java
+++ modul2/PewarisanTugas/mainMhsFak.java
@@ -1,7 +1,16 @@
 package modul2.PewarisanTugas;
 
 public class mainMhsFak {
-    Mahasiswa m=new Mahasiswa();
+    public static void main(String[] args) {
+        Fakultas fak=new Fakultas();
 
-    
+        fak.setNama("Irsyad");
+        fak.setNim("090202342365");
+        fak.setNISN(123405);
+        fak.setFakultas("FST");
+        fak.setKdProdi("SIF");
+        fak.setSemester(3);
+        System.out.println();
+        fak.showData();
+    }
 }
